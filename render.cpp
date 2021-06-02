c1aa357f87843fbd516b22e2f380a9f1b503359f
Johannes Burström <johannes@ljud.org>
2021-06-02 13:57:20 +0200

    Trill: Avoid sending multiple zero-touch messages

--- render.cpp
+++ render.cpp
@@ -46,6 +46,7 @@ Pipe gTrillPipe;
 
 static std::vector<std::string> gTrillAcks;
 static std::vector<std::pair<std::string,Trill*>> gTouchSensors;
+int gTouchSensorsStatus[16] = {0};
 // how often to read the cap sensors inputs.
 float touchSensorSleepInterval = 0.007;
 
@@ -956,11 +957,17 @@ void render(BelaContext *context, void *userData)
 			} else if(Trill::CENTROID == mode)
 			{
 				if(touchSensor.is1D()) {
-					libpd_start_message(2 * touchSensor.getNumTouches() + 1);
-					libpd_add_float(touchSensor.getNumTouches());
-					for(int i = 0; i < touchSensor.getNumTouches(); i++) {
-						libpd_add_float(touchSensor.touchLocation(i));
-						libpd_add_float(touchSensor.touchSize(i));
+
+					//Avoid sending multiple zero messages
+					if (touchSensor.getNumTouches() != gTouchSensorsStatus[idx] 
+							|| gTouchSensorsStatus[idx] > 0) {
+						gTouchSensorsStatus[idx] = touchSensor.getNumTouches();
+						libpd_start_message(2 * touchSensor.getNumTouches() + 1);
+						libpd_add_float(touchSensor.getNumTouches());
+						for(int i = 0; i < touchSensor.getNumTouches(); i++) {
+							libpd_add_float(touchSensor.touchLocation(i));
+							libpd_add_float(touchSensor.touchSize(i));
+						}
 					}
 				} else if (touchSensor.is2D()) {
 					int numTouches = touchSensor.compoundTouchSize() > 0;
